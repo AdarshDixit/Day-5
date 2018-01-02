34b2ff8e4f0dde683efe983286e6b0622877eb77
Learning <Learning@DIN86000566.corp.capgemini.com>
2018-01-02 16:43:58 +0530

    attribute based routing

---
+++ Day 4/Day 4/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Home/{id?}")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        
+    }
+}
